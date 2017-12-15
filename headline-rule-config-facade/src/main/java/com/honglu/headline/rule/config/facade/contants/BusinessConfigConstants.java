21b8afdd80b15632d974945303897b7a61801767
xiangxianjin <xiangxianjin@xiaoniu.com>
2017-12-15 14:11:45 +0800

    业务参数常量

---
+++ headline-rule-config-facade/src/main/java/com/honglu/headline/rule/config/facade/contants/BusinessConfigConstants.java
@@ -0,0 +1,41 @@
+package com.honglu.headline.rule.config.facade.contants;
+
+/**
+ * 业务参数配置常量
+ *
+ * @author xiangxianjin
+ * @date 2017-12-15 13:51
+ */
+public class BusinessConfigConstants {
+
+    /**
+     * 每日金币阅读收益上限
+     */
+    public static final String ACCOUNT_DAY_GOLD_CELL = "ACCOUNT_DAY_GOLD_CELL";
+
+    /**
+     * 每日分润收益上限
+     */
+    public static final String ACCOUNT_DAY_PROFIT_GOLD_CELL = "ACCOUNT_DAY_PROFIT_GOLD_CELL";
+
+    /**
+     * 每日红包领取标准
+     */
+    public static final String ACCOUNT_DAY_RED_LIMIT = "ACCOUNT_DAY_RED_LIMIT";
+
+    /**
+     * 每日翻倍权益领取标准
+     */
+    public static final String ACCOUNT_DAY_DOUBLE_LIMIT = "ACCOUNT_DAY_DOUBLE_LIMIT";
+
+    /**
+     * 注册成功发放红包
+     */
+    public static final String USER_REGISTER_REDPACKAGE = "USER_REGISTER_REDPACKAGE";
+
+    /**
+     * 金币零钱兑换比例配置 1元 = n金币
+     */
+    public static final String ACCOUNT_EXCHANGE_MULTIPLE = "ACCOUNT_EXCHANGE_MULTIPLE";
+
+}
